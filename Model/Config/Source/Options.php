9525f93c93a170ebd739235c7885344ce0d61824
gexen2 <inghedenadam@gmail.com>
2019-11-18 15:59:07 +0100

    First commit

---
+++ Model/Config/Source/Options.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Crealevant\AddToWishlistNotRedirect\Model\Config\Source;
+
+class Options implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 0, 'label' => __('No')],
+            ['value' => 1, 'label' => __('Yes')]
+        ];
+    }
+}
