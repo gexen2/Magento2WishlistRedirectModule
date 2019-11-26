7b8650af6669a4e8535290667dfc24b44b9c2fc4
gexen2 <inghedenadam@gmail.com>
2019-11-26 11:55:08 +0100

    Added: helper files: Work in progress: Hot code

---
+++ Controller/Index/Config.php
@@ -0,0 +1,32 @@
+<?php
+/*
+namespace Crealevant\AddToWishlistNotRedirect\Controller\Index;
+
+class Config extends \Magento\Framework\App\Action\Action
+{
+
+	protected $helperData;
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Crealevant\AddToWishlistNotRedirect\Helper\Data $helperData
+
+	)
+	{
+		$this->helperData = $helperData;
+		return parent::__construct($context);
+	}
+
+	public function execute()
+	{
+
+		// TODO: Implement execute() method.
+
+		echo $this->helperData->getGeneralConfig('enable');
+		echo $this->helperData->getGeneralConfig('display_text');
+		exit();
+
+	}
+}
+*/
+// https://www.mageplaza.com/magento-2-module-development/create-system-xml-configuration-magento-2.html
